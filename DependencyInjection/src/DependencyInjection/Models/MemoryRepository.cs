ecca9860bf0dd055a51d49706f21e5f7a5891b41
cortez-scr <cortez.scr@gmail.com>
2017-04-11 14:21:48 -0500

    First Commit

---
+++ DependencyInjection/src/DependencyInjection/Models/MemoryRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Models
+{
+    public class MemoryRepository:IRepository
+    {
+        private IModelStorage storage;
+        private string guid = System.Guid.NewGuid().ToString();
+        public MemoryRepository(IModelStorage modelStore) {
+            storage = modelStore;
+
+            new List<Product> {
+                new Product { Name="Kayak" , Price =275m},
+                new Product { Name="LifeJacket" , Price =48.95m},
+                new Product { Name="Soccer Ball" , Price =19.50m}
+            }.ForEach(p => AddProduct(p));
+
+        }
+
+        public IEnumerable<Product> Products => storage.Items;
+
+        public Product this[string name] => storage[name];
+
+        public void AddProduct(Product product) => storage[product.Name] = product;
+
+        public void DeleteProduct(Product product) => storage.RemoveItem(product.Name);
+
+        public override string ToString()
+        {
+            return guid;
+        }
+
+
+
+    }
+}
---
+++ DependencyInjection/src/DependencyInjection/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using DependencyInjection.Infrastructure;
+using DependencyInjection.Models;
+
+namespace DependencyInjection
+{
+    public class Startup
+    {
+
+        private IHostingEnvironment env;
+        public Startup(IHostingEnvironment hostEnv) { env = hostEnv; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IRepository,MemoryRepository>();
+            services.AddTransient<IModelStorage, DictionaryStorage>();
+            services.AddTransient<ProductTotalizer>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
